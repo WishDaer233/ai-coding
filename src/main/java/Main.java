c679d223181217de23f04ade65e498507d1837d7
WishDaer233 <1627703574@qq.com>
2026-06-17 09:12:53 +0800

    测试

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("aaa");
-        System.out.println("bbb");
+        //让所有分支在最新节点
+        System.out.println("测试");
     }
 }
